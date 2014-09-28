c2bbd55135e7c32923f712c50f642b09028f0266
Tostito <tostitogaming@live.com>
2014-09-28 16:25:31 -0500

    files removed

---
+++ README.md
@@ -0,0 +1,15 @@
+SE-RemovePlayers
+===============
+**INTENDED FOR DEDICATED SERVERS
+
+The author of this script is not responsible for any damage to the game save or the machine it is run on.
+
+Player removal if no blocks owned.
+
+This powershell script is used by changing the variables at the top of the 'ps1' file to match your server settings.
+
+The default settings will delete players from the game if they do not own any blocks.
+
+Be sure to shut your space engineers server down before running this script!!!
+
+Use the windows shortcut provided to run. Be sure the shortcut and the script file are in the same folder.
---
+++ SEremoveplayers.ps1
@@ -0,0 +1,36 @@
+﻿$filePath = 'your save path here\SANDBOX_0_0_0_.sbs'
+$filePath2 = 'your save path here\SANDBOX.sbc'
+
+# ===== only change the above values
+
+[xml]$myXML = Get-Content $filePath
+$ns = New-Object System.Xml.XmlNamespaceManager($myXML.NameTable)
+$ns.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
+
+[xml]$myXML2 = Get-Content $filePath2
+$ns2 = New-Object System.Xml.XmlNamespaceManager($myXML2.NameTable)
+$ns2.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
+
+
+# remove players who dont own anything    
+
+    $nodePIDs = $myXML2.SelectNodes("//AllPlayers/PlayerItem"  , $ns2) 
+    ForEach($node in $nodePIDs){
+        $nodeid = $node.PlayerId
+        $nodeOwns = $myXML.SelectNodes("//SectorObjects/MyObjectBuilder_EntityBase[(@xsi:type='MyObjectBuilder_CubeGrid')]/CubeBlocks/MyObjectBuilder_CubeBlock[Owner='$nodeid']"  , $ns).Count
+            If($nodeOwns -eq 0){
+              $selectdelete = $myXML2.SelectSingleNode("//Factions/Factions/MyObjectBuilder_Faction/Members/MyObjectBuilder_FactionMember[PlayerID='$nodeid']" , $ns2)
+              $selectdelete.ParentNode.RemoveChild($selectdelete)
+              $selectdelete = $myXML2.SelectSingleNode("//Factions/Players/dictionary/item[Key='$nodeid']", $ns2)
+              $selectdelete.ParentNode.RemoveChild($selectdelete)
+              $selectdelete = $myXML2.SelectSingleNode("//Factions/Factions/MyobjectBuilder_Faction/JoinRequests/MyObjectBuilder_FactionMember[PlayerID='$nodeid']" , $ns2)
+              $selectdelete.ParentNode.RemoveChild($selectdelete)
+              $node.ParentNode.RemoveChild($node)
+            }
+        
+           
+
+    }
+
+    $myXML.Save($filePath)
+
